f4fe2bea3c3ad1b99f3f5a393201063d0a97acd9
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-18 20:22:55 +0700

    Database Migrations

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 
 from models import db
 
@@ -7,6 +8,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 db.init_app(app)
+
+migrate = Migrate(app, db)
 
 
 @app.route('/')
