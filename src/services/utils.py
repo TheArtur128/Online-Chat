8b0232533d2ebec069c8295932b2b3cbe4298092
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-21 21:34:32 +0700

    Range class for ASCII characters

--- src/services/utils.py
+++ src/services/utils.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from marshmallow.validate import Length
 
 from models import db
@@ -6,3 +8,18 @@ from models import db
 def create_length_validator_by_model_column(model: db.Model, column: str) -> Length:
     return Length(max=getattr(model, column).comparator.type.length)
 
+
+@dataclass(frozen=True)
+class ASCIIRange:
+    start: int = 0
+    end: int = 127
+    step: int = 1
+
+    def __post_init__(self):
+        if self.step == 0:
+            raise ValueError("step must not be zero")
+
+    def __iter__(self) -> iter:
+        return iter(
+            (chr(symbol_index) for symbol_index in range(self.start, self.end, self.step))
+        )
