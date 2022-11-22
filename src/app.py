1e858d82fc31b5cdc5762ddead897ac488f7a1a6
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-22 22:05:19 +0700

    Rest API Structure

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
 
+from api import api_blueprint
 from models import db
 from views import view_blueprint
 
@@ -8,6 +9,7 @@ from views import view_blueprint
 app = Flask(__name__)
 app.config.from_object('config')
 
+app.register_blueprint(api_blueprint, url_prefix='/api')
 app.register_blueprint(view_blueprint)
 
 db.init_app(app)
