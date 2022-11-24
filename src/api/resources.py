b5ee64bbf8ddda6f0eac13a69b28ec884e8fb722
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-24 22:18:15 +0700

    UserResource uses the schemas set in the class and not the input

--- src/api/resources.py
+++ src/api/resources.py
@@ -8,11 +8,6 @@ from schemes import BaseUserSchema, FullUserSchema
 
 
 class UserResource(Resource):
-	_default_user_schema: Schema = BaseUserSchema(only=('user_url_token'))
-
-	def __init__(self, user_schema: Optional[Schema] = None):
-		self._user_schema = user_schema if user_schema else self._default_user_schema
-		self._user_schema.many = True
 	_user_serialization_schema: Schema = BaseUserSchema(only=('user_url_token', ))
 	_user_deserialization_schema: Schema = FullUserSchema(exclude=('password', ))
 
