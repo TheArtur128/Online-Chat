74f1a08fc2536d243a2cd28b8d5268438887b87d
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-25 20:29:34 +0700

    User registration resource

--- src/api/resources.py
+++ src/api/resources.py
@@ -4,12 +4,15 @@ from flask_restful import Resource
 from flask import request
 from marshmallow import Schema
 
-from services.middlewares import ServiceErrorMiddleware
+from models import db
 from schemes import FullUserSchema
+from services.middlewares import ServiceErrorMiddleware
+from services.authorization_services import DBUserRegistrar
 
 
 class UserResource(Resource):
 	user_schema: Schema = FullUserSchema(exclude=('password', 'password_hash'))
+	user_registrar: Callable[[dict], any] = DBUserRegistrar(db)
 
 	@ServiceErrorMiddleware().decorate
 	def get(self):
@@ -17,3 +20,9 @@ class UserResource(Resource):
 			self.user_schema.load(request.json, many=True),
 			many=True
 		)
+
+	@ServiceErrorMiddleware().decorate
+	def post(self):
+		self.user_registrar(request.json)
+
+		return dict(), 201
