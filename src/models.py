93cb665db6ab82e7ade447277adadc78c18c4a10
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-26 23:19:57 +0700

    Removing the indication of the model name in the model fields

--- src/models.py
+++ src/models.py
@@ -49,8 +49,8 @@ class _FormattedUrlModelMixin:
 class User(db.Model, _FormattedUrlModelMixin):
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_url_token = db.Column(db.String(32), nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    url_token = db.Column(db.String(32), nullable=False, unique=True)
     password_hash = db.Column(db.String(1024), nullable=False)
     public_username = db.Column(db.String(32))
     avatar_path = db.Column(db.String(512))
@@ -59,24 +59,17 @@ class User(db.Model, _FormattedUrlModelMixin):
     chats = db.relationship('Chat', secondary=chat_member_table, back_populates='members')
     roles = db.relationship('ChatRole', secondary=chat_member_role_table, back_populates='actors')
 
-    @property
-    def name(self) -> str:
-        return self.public_username if self.public_username else self.user_url_token
 
-    def __repr__(self) -> str:
-        return f"User{' ' + self.public_username if self.public_username else ''}(id={self.user_id}, url={self.user_url_token})"
 
-    def get_id(self) -> int:
-        return self.user_id
 
 
 class Chat(db.Model):
     __tablename__ = 'chats'
 
-    chat_id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
-    chat_url_token = db.Column(db.String(32), nullable=False, unique=True)
-    public_chat_name = db.Column(db.String(32))
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    url_token = db.Column(db.String(32), nullable=False, unique=True)
+    name = db.Column(db.String(32))
     description = db.Column(db.String(256))
 
     members = db.relationship('User', secondary=chat_member_table, back_populates='chats')
@@ -93,9 +86,9 @@ class Chat(db.Model):
 class ChatRole(db.Model):
     __tablename__ = 'chat_roles'
 
-    chat_role_id = db.Column(db.Integer, primary_key=True)
-    chat_id = db.Column(db.Integer, db.ForeignKey(Chat.chat_id))
-    chat_role_name = db.Column(db.String(16), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey(Chat.id))
+    name = db.Column(db.String(16), nullable=False)
     description = db.Column(db.String(256))
     hex_rgb_color = db.Column(db.String(6), nullable=False, default='FFFFFF')
     role_customization_rights = db.Column(db.Boolean, nullable=False, default=False)
@@ -107,15 +100,15 @@ class ChatRole(db.Model):
     actors = db.relationship('User', secondary=chat_member_role_table, back_populates='roles')
 
     def __repr__(self) -> str:
-        return f"ChatRole {self.chat_role_name} of {self.chat.chat_url_token} chat with rights(customization={self.role_customization_rights}, ban={self.user_ban_rights}, message_pinning={self.message_pinning_rights}, styling={self.styling_rights})"
+        return f"ChatRole {self.name} of {self.chat.url_token} chat with rights(customization={self.role_customization_rights}, ban={self.user_ban_rights}, message_pinning={self.message_pinning_rights}, styling={self.styling_rights})"
 
 
 class ChatMessage(db.Model):
     __tablename__ = 'chat_messages'
 
-    chat_message_id = db.Column(db.Integer, primary_key=True)
-    chat_id = db.Column(db.Integer, db.ForeignKey(Chat.chat_id), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey(Chat.id), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(User.id))
     creation_time = db.Column(db.DateTime, default=datetime.now)
     message = db.Column(db.String(1024), nullable=False)
     pinned = db.Column(db.Boolean, nullable=False, default=False)
