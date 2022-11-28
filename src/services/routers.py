eea654df632cafebcc8275e553b3657e8ff00eab
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-11-28 18:30:20 +0700

    Router interface base implementation class

--- src/services/routers.py
+++ src/services/routers.py
@@ -5,3 +5,16 @@ class IRouter(ABC):
     @abstractmethod
     def __call__(self, data: dict) -> any:
         pass
+
+
+class Router(IRouter, ABC):
+    def __call__(self, data: dict) -> any:
+        return self._handle_cleaned_data(self._get_cleaned_data_from(data))
+
+    @abstractmethod
+    def _get_cleaned_data_from(self, data: dict) -> dict:
+        pass
+
+    @abstractmethod
+    def _handle_cleaned_data(self, data: dict) -> any:
+        pass
