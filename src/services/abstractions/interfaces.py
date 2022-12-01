57bf4606df56a221983513379c4d016b172aae26
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-01 22:35:43 +0700

    Jwt serializer interface

--- src/services/abstractions/interfaces.py
+++ src/services/abstractions/interfaces.py
@@ -6,3 +6,10 @@ class IRouter(ABC):
     @abstractmethod
     def __call__(self, data: dict | Iterable) -> any:
         pass
+
+
+class IJWTCoder(ABC):
+    @abstractmethod
+    def encode(self, data: dict) -> str:
+        pass
+
