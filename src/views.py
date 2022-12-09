8aa3ffc4c403a66f53eb911a4a10c044127e1e27
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-09 22:39:25 +0700

    Template for future registration and authorization views

--- src/views.py
+++ src/views.py
@@ -7,3 +7,13 @@ view_blueprint = Blueprint('views', __name__)
 @view_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+
+@view_blueprint.route('/registration')
+def registration():
+    pass
+
+
+@view_blueprint.route('/login')
+def authorization():
+    pass
