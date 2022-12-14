3669764b0cb386d392c45952d7d3a44ebecf6406
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-14 22:39:07 +0700

    JSONResponseFormatter class for DocumentaryError

--- src/infrastructure/middlewares.py
+++ src/infrastructure/middlewares.py
@@ -2,10 +2,12 @@ from typing import Callable
 
 from flask import request
 from flask_middlewares import Middleware
+from flask_middlewares.standard.error_handling import JSONResponseErrorFormatter, TypeErrorHandler
 from jwt import InvalidTokenError
 
-from infrastructure.errors import AccessTokenInvalidError
+from infrastructure.errors import AccessTokenInvalidError, DocumentaryError
 from tools.jwt_serializers import IJWTDecoder
+from tools.utils import get_status_code_from_error
 
 
 class AccessTokenRequiredMiddleware(Middleware):
@@ -24,3 +26,19 @@ class AccessTokenRequiredMiddleware(Middleware):
             raise AccessTokenInvalidError('Access token is invalid')
 
         return route(*args, **kwargs)
+
+
+class DocumentaryErrorJSONResponseFormatter(JSONResponseErrorFormatter, TypeErrorHandler):
+    _correct_error_types_to_handle = (DocumentaryError, )
+    _get_status_code_from = staticmethod(get_status_code_from_error)
+
+    def _get_response_body_from(self, error: DocumentaryError) -> dict:
+        response_body = {'error-type': type(error).__name__}
+
+        if error.message:
+            response_body['message'] = error.message
+
+        if error.document:
+            response_body['detail'] = error.document
+
+        return response_body
