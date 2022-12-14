2350388cf61542cb42a7fcb569c45f39a3ea01f7
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-14 22:58:30 +0700

    Handling DocumentaryError by the global middleware

--- src/middleware_config.py
+++ src/middleware_config.py
@@ -3,16 +3,19 @@ from flask_middlewares.standard.status_code import AbortBadStatusCodeMiddleware,
 from flask_middlewares.standard.sql_alchemy import SQLAlchemySessionFinisherMiddleware
 
 from config import DEFAULT_JWT_SERIALIZATOR_FACTORY
-from infrastructure.middlewares import AccessTokenRequiredMiddleware
+from infrastructure.middlewares import AccessTokenRequiredMiddleware, DocumentaryErrorJSONResponseFormatter
 from orm import db
 from tools.utils import get_status_code_from_error
 
 
 GLOBAL_MIDDLEWARES = (
-    CustomHandlerErrorMiddleware(CustomJSONResponseErrorFormatter(
-        (Exception, ),
-        get_status_code_from_error,
-        is_format_type=False
+    CustomHandlerErrorMiddleware((
+        DocumentaryErrorJSONResponseFormatter(),
+        CustomJSONResponseErrorFormatter(
+            (Exception, ),
+            get_status_code_from_error,
+            is_format_type=False
+        )
     )),
     SQLAlchemySessionFinisherMiddleware(db),
     AccessTokenRequiredMiddleware(DEFAULT_JWT_SERIALIZATOR_FACTORY())
