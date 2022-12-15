56f8b7d24e673bc8505977484ef53b2a993c1735
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-15 19:06:01 +0700

    Protected repository annotation class

--- src/services/repositories.py
+++ src/services/repositories.py
@@ -25,7 +25,57 @@ class IRepository(ABC):
         pass
 
 
-class SQLAlchemyRepository(IRepository, ABC):
+class _RepositoryAnnotation:
+    def __init__(self, repository_type: type, supported_types: Iterable[type]):
+        self.repository_type = repository_type
+        self.supported_types = supported_types
+
+    @property
+    def supported_types(self) -> tuple:
+        return self._supported_types
+
+    @supported_types.setter
+    def supported_types(self, supported_types: Iterable) -> None:
+        self._supported_types = tuple(supported_types)
+
+        if not self._supported_types:
+            raise AttributeError("The length of supported_types must be greater than zero")
+
+    @property
+    def centered_supported_types(self) -> Union:
+        return Union[*self.supported_types]
+
+    def __repr__(self) -> str:
+        return "{repository_type_name}[{supported_type_part}]".format(
+            repository_type_name=self.repository_type.__name__,
+            supported_type_part=self.__get_formatted_supported_types()
+        )
+
+    def __instancecheck__(self, instance: object) -> bool:
+        return (
+            isinstance(instance, self.repository_type)
+            and (
+                not self.supported_types or any(
+                    issubclass(instance_type, self.centered_supported_types)
+                    for instance_type in instance.supported_storage_types
+                )
+            )
+        )
+
+    def __get_formatted_supported_types(self) -> str:
+        supported_type_line = str(self.centered_supported_types)
+
+        return (
+            supported_type_line[supported_type_line.index('[') + 1:-1].replace(',', ' |')
+            if (
+                hasattr(self.centered_supported_types, '__origin__')
+                and self.centered_supported_types.__origin__ is Union
+            )
+            else self.centered_supported_types.__name__
+        )
+
+
+class SQLAlchemyRepository(Repository, ABC):
     _sqlalchemy_model: db.Model
 
     def __init__(self, session: SQLAlchemy):
