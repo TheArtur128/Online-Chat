11c106b5d11fb6424257bd7be71169dc5ee30713
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-22 22:58:57 +0700

    AccessTokenRequiredMiddleware delegates token validity check to TokenPromiser

--- src/infrastructure/middlewares.py
+++ src/infrastructure/middlewares.py
@@ -5,22 +5,26 @@ from jwt import InvalidTokenError
 
 from infrastructure.controllers import ControllerResponse
 from infrastructure.errors import AccessTokenInvalidError
-from services.serializers import IDecoder
+from services.errors import MissingTokenError, InvalidTokenError
+from services.tokens import TokenPromiser
 
 
 class AccessTokenRequiredMiddleware(Middleware):
-    def __init__(self, token_decoder: IDecoder, access_token_getter: Callable[[], Optional[str]]):
-        self.token_decoder = token_decoder
+    def __init__(
+        self,
+        access_token_promiser: TokenPromiser,
+        access_token_getter: Callable[[], Optional[str]]
+    ):
+        self.access_token_promiser = access_token_promiser
         self.access_token_getter = access_token_getter
 
     def call_route(self, route: Callable, *args, **kwargs) -> any:
         access_token = self.access_token_getter()
 
-        if not access_token:
-            raise AccessTokenInvalidError("Access token is missing")
-
         try:
-            self.token_decoder.decode(access_token)
+            self.access_token_promiser(access_token)
+        except MissingTokenError:
+            raise AccessTokenInvalidError("Access token is missing")
         except InvalidTokenError:
             raise AccessTokenInvalidError("Access token is invalid")
 
