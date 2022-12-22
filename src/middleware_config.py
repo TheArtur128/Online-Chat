4fb084855496b1a4c5dd2698358a2950a5c60ef1
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-22 23:00:34 +0700

    Specifying the middleware for blueprint api

--- src/middleware_config.py
+++ src/middleware_config.py
@@ -4,18 +4,15 @@ from flask_middlewares.standard.sql_alchemy import SQLAlchemySessionFinisherMidd
 
 from config import DEFAULT_JWT_SERIALIZATOR_FACTORY
 from frameworks.flask import get_flask_response_by_controller_response, FlaskAccessTokenGetter
+from infrastructure.errors import ResorceError, AccessTokenInvalidError
 from infrastructure.middlewares import ControllerResponseFormatterMiddleware, AccessTokenRequiredMiddleware
 from orm import db
+from services.tokens import TokenPromiser
 from tools.error_handlers import DocumentaryErrorJSONResponseFormatter
 from tools.utils import get_status_code_from_error
 
 
 IS_GLOBAL_MIDDLEWARES_HIGHER = False
-
-MIDDLEWARES = (
-    AbortBadStatusCodeMiddleware(),
-    StatusCodeRedirectorMiddleware('views.authorization', 403)
-)
 
 GLOBAL_MIDDLEWARES = (
     ControllerResponseFormatterMiddleware(get_flask_response_by_controller_response),
@@ -36,4 +33,12 @@ GLOBAL_MIDDLEWARES = (
 
 MIDDLEWARE_ENVIRONMENTS = {
     'api': {'USE_FOR_BLUEPRINT': True}
+    'views': {
+        'USE_FOR_BLUEPRINT': True,
+        'IS_GLOBAL_MIDDLEWARES_HIGHER': False,
+        'MIDDLEWARES': (
+            AbortBadStatusCodeMiddleware(),
+            StatusCodeRedirectorMiddleware('views.authorization', 403)
+        ),
+    }
 }
