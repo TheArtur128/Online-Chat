dfa3727aac7e2655912df4a1d69c59b327933a92
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-03 22:28:48 +0700

    Middleware of redirection by status code

---
+++ src/middlewares/middlewares.py
@@ -0,0 +1,17 @@
+from flask import request
+from pyhandling import *
+from pyhandling.annotations import checker_of, decorator
+
+from adapters.tokens import JWTSerializator
+from services.tokens import validate_access_token
+from tools.utils import event_decorator
+
+
+def redirect_on_status_code_that(status_code_checker: checker_of[int], url_to_redirect: str) -> decorator:
+    return (returnly |then>> next_action_decorator_of)(
+        status_code_from
+        |then>> on_condition(
+            status_code_checker,
+            url_to_redirect >= close(redirect) |then>> eventually
+        )
+    )
