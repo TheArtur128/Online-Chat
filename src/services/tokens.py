10708a54bdb3aeb13e2c614dec62a859e180d432
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-03 22:20:15 +0700

    Replacing the TokenPromiser hierarchy with validate_access_token

--- src/services/tokens.py
+++ src/services/tokens.py
@@ -8,21 +8,13 @@ class TokenDecoderResult(Enum):
     INCORRECT = auto()
 
 
-class ITokenPromiser(ABC):
-    @abstractmethod
-    def __call__(self, token: Optional[str]) -> None:
-        pass
 token_coder = Callable[[dict], str]
 token_decoder = Callable[[str], dict | TokenDecoderResult]
 
 
-class TokenPromiser(ITokenPromiser):
-    def __init__(self, token_decoder: ITokenDecoder):
-        self.token_decoder = token_decoder
+def validate_access_token(token: Optional[str], token_decoder: token_decoder) -> None:
+    if not token:
+        raise AccessTokenError("Access token is missing")
 
-    def __call__(self, token: Optional[str]) -> None:
-        if not token:
-            raise MissingTokenError("Token is missing")
-        
-        if self.token_decoder.decode(token) is TokenDecoderResult.INCORRECT:
-            raise InvalidTokenError("Token is invalid")
+    if token_decoder(token) is TokenDecoderResult.INCORRECT:
+        raise AccessTokenError("Access token is invalid")
