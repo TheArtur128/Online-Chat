8ff63ceea61e10f3f384a0f227b420cc7398541c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-03 22:19:28 +0700

    Replacing interfaces with annotations

--- src/services/tokens.py
+++ src/services/tokens.py
@@ -1,34 +1,19 @@
-from abc import ABC, abstractmethod
 from typing import Optional
 from enum import Enum, auto
 
-from services.errors import MissingTokenError, InvalidTokenError
-
-
-class ITokenCoder(ABC):
-    @abstractmethod
-    def encode(self, data: dict) -> str:
-        pass
+from services.errors import AccessTokenError
 
 
 class TokenDecoderResult(Enum):
     INCORRECT = auto()
 
 
-class ITokenDecoder(ABC):
-    @abstractmethod
-    def decode(self, token: str) -> dict | TokenDecoderResult:
-        pass
-
-
-class ITokenSerializator(ITokenCoder, ITokenDecoder, ABC):
-    pass
-
-
 class ITokenPromiser(ABC):
     @abstractmethod
     def __call__(self, token: Optional[str]) -> None:
         pass
+token_coder = Callable[[dict], str]
+token_decoder = Callable[[str], dict | TokenDecoderResult]
 
 
 class TokenPromiser(ITokenPromiser):
