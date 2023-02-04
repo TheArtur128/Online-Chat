660cd269e6f0c14c7786302728499abcdee26446
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-04 22:04:50 +0700

    Import already used resources

--- src/middlewares/config.py
+++ src/middlewares/config.py
@@ -1,4 +1,7 @@
 from flask_middlewares.tools import get_status_code_from
+from flask_middlewares.tools import BinarySet
+
+from middlewares import required_access_token, redirect_on_status_code_that
 
 
 MIDDLEWARE_ENVIRONMENTS = {
