670328998f574d66ed802c7bfee8438fae23e622
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-04 22:16:26 +0700

    Actual registration services

--- src/services/authorization.py
+++ src/services/authorization.py
@@ -1,6 +1,6 @@
 from typing import Protocol, runtime_checkable
 
-from services.errors import AccountAlreadyExistsError
+from services.errors import RegistrationError
 from services.repositories import IRepository
 from services.tokens import token_coder
 from tools.utils import get_time_after
@@ -15,24 +15,34 @@ class Session(Protocol):
 class Profile(Protocol):
     token: str
 
-def register_account(account: Account, repository: IRepository[Account]) -> None:
-    if repository.get_by(url_token=account.url_token) is not None:
-        raise AccountAlreadyExistsError(
-            f"Account with \"{account.url_token}\" url token already exists"
 
 @runtime_checkable
 class Account(Protocol):
     profile: Profile
     session: Session
+
+
+def register_account(
+    account: Account,
+    account_repository: IRepository[Account],
+    profile_repository: IRepository[Profile],
+    session_validator: checker_of[Session]
+) -> None:
+    if profile_repository.get_by(token=account.profile.token) is not None:
+        raise RegistrationError(
+            f"{account.profile.name}'s profile already exists"
         )
 
-    repository.add(account)
+    if not session_validator(account.session):
+        raise RegistrationError(f"Session for {account.profile.name}'s account is invalid")
+
+    account_repository.add(account)
 
 
 def create_token_for(account: Account, coder: token_coder, life_minutes: int | float) -> str:
     return coder(
         dict(
-            url_token=account.url_token,
+            token=account.token,
             exp=get_time_after(life_minutes, is_time_raw=True)
         )
-    )
+
