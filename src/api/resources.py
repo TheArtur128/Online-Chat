3df88e6626221e25c042dbcbe57accad2580b3e0
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-18 17:13:07 +0700

    User resource

--- src/api/resources.py
+++ src/api/resources.py
@@ -1,6 +1,18 @@
 from abc import ABC
+from functools import partial
+
 from flask_restful import Resource
+from pyhandling import then, to, ArgmuntPack, unpackly, close, post_partial
 from pyhandling.annotations import decorator
+
+from adapters.repositories import SQLAlchemyRepository
+from infrastructure.controllers import convert_by, search_in
+from maps.schemes import UserSchema, user_schema_without_passwords
+from maps.scultures import account_sculture_from, original_from, profile_sculture_from
+from orm import db
+from orm.models import User
+from services.authorization import register_account, is_session_timed_out
+from tools.utils import data_additing_decorator_by, dict_value_map
 
 
 class DecoratedResourceMixin(Resource, ABC):
@@ -17,3 +29,25 @@ class DecoratedResourceMixin(Resource, ABC):
             )
 
         super().__init__()
+
+
+class UserResource(DecoratedResourceMixin):
+    _decorator = data_additing_decorator_by(request.json)
+
+    get = (
+        (convert_by |to| user_schema_without_passwords(many=True))
+        |then>> (search_in |to| SQLAlchemyRepository(db, User))
+        |then>> (dict_value_map |to| user_schema_without_passwords().dump)
+    )
+
+    post = (
+        (convert_by |to| UserSchema(exclude=["password"], many=False))
+        |then>> ArgmuntPack
+        |then>> unpackly(User) # Will be expanded
+        |then>> account_sculture_from
+        |then>> close(register_account, closer=post_partial)(
+            ConvertingRepository(SQLAlchemyRepository(db, User), account_sculture_from, original_from),
+            ConvertingRepository(SQLAlchemyRepository(db, User), profile_sculture_from, original_from),
+            is_session_timed_out
+        )
+    )
