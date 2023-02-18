164dce61e65377fb1aa87b4b6e570c9161463c82
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-18 17:06:29 +0700

    Correct nesting for format dict header

--- src/tools/formatters.py
+++ src/tools/formatters.py
@@ -21,13 +21,13 @@ def convert_error_report_to_dict(
 
 
 def format_dict(
-        data: dict,
-        *,
-        line_between_elements: str = ', ',
-        line_between_key_and_value: str = ': ',
-        key_changer: Callable[[str], any] = lambda key: key,
-        value_changer: Callable[[any], any] = lambda value: value,
-    ) -> str:
+    data: dict,
+    *,
+    line_between_elements: str = ', ',
+    line_between_key_and_value: str = ': ',
+    key_changer: Callable[[str], any] = lambda key: key,
+    value_changer: Callable[[any], any] = lambda value: value,
+) -> str:
     return line_between_elements.join(
         "{key_part}{line_between_key_and_value_part}{value_part}".format(
             key_part=str(key_changer(key)),
