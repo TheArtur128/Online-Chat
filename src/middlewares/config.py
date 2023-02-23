347ea91503b7ec5f82efbf8e75138b7670c5404c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-23 22:44:22 +0700

    Using the correct middleware config field names

--- src/middlewares/config.py
+++ src/middlewares/config.py
@@ -10,7 +10,7 @@ MIDDLEWARE_ENVIRONMENTS = {
     },
     "redirecting": {
         "USE_FOR_BLUEPRINT": "views",
-        "MIDDLEWARE_VIEW_NAMES": BinarySet(non_included=["views.authorization"]),
+        "VIEW_NAMES": BinarySet(non_included=["views.authorization"]),
         "MIDDLEWARES": [
             redirect_on_status_code_that(
                 operation_by('==', 403),
